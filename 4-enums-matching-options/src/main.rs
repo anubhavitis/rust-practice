dc6211e40f6b5e46e8ac33767856d4acd77acd05
Anubhav Singhal <ss.lfsgd@gmail.com>
2023-11-04 13:37:55 +0530

    first enums and match run

--- 4-enums-matching-options/src/main.rs
+++ 4-enums-matching-options/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+
+// Enums are way to declare values of similar type.
+// Like this, we can create stuff with Day keyword
+// and can use any value inside day to work on.
+#[derive(Debug)]
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thrusday,
+    Friday,
+    Saturday,
+    Sunday
 }
+
+fn type_of_day(day:&Day) -> &str {
+    let resp:&str = match day {
+        Day::Monday | Day:: Tuesday => "Boring days",
+        Day::Wednesday | Day::Thrusday => "Exhaustive & waiting for weekend days",
+        Day::Friday => "Happy workday",
+        _ => "Sleeping weekend" // This is like default, we must always have a default for match
+    };
+    resp
+}
+
+fn main() {
+    let today = Day::Wednesday; 
+    println!("It's {:?}, there it's: {:?}",today, type_of_day(&today))
+}
+
